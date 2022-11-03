0c5fc279f9e09b1d9afe7f26577f08cfd38ef2a7
Khang Nguyen <nguyentruongkhang.1509@gmail.com>
2022-11-03 17:56:00 +0700

    ///

--- js/utils/carousel.js
+++ js/utils/carousel.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
   $('.owl-carousel').owlCarousel();
 });
+
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
+const webpack = require('webpack');
+
+//...
+plugins: [
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+  }),
+];
+//...
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 // vite.config.js
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        destinationDetail: resolve(__dirname, "destination-detail.html"),
+        main: resolve(__dirname, 'index.html'),
+        destinationDetail: resolve(__dirname, 'destination-detail.html'),
       },
     },
   },
